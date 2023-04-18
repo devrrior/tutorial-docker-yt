c276c72e3fbe52005b072e4527339076f42d1128
Fernando Guerrero <devrrior@gmail.com>
2023-04-18 10:04:23 -0600

    Update running port

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ python manage.py wait_for_db --settings=config.settings.production
 python manage.py migrate --settings=config.settings.production
 
 echo 'Running server...'
-gunicorn --env DJANGO_SETTINGS_MODULE=config.settings.production config.wsgi:application --bind 0.0.0.0:8000
+gunicorn --env DJANGO_SETTINGS_MODULE=config.settings.production config.wsgi:application --bind 0.0.0.0:$PORT
